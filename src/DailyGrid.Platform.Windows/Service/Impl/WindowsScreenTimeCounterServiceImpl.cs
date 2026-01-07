52e612d6fb22c615d9e58f416081e2598f5d9180
ZiDiYao <zidiyaocanada@outlook.com>
2026-01-07 16:07:52 -0500

    update IOC

--- src/DailyGrid.Platform.Windows/Service/Impl/WindowsScreenTimeCounterServiceImpl.cs
+++ src/DailyGrid.Platform.Windows/Service/Impl/WindowsScreenTimeCounterServiceImpl.cs
@@ -1,2 +1,25 @@
-namespace DailyGrid.Platform.Windows.Service.Impl;
+using DailyGrid.Platform.Windows.dto;
 
+namespace DailyGrid.Platform.Windows.Service.Impl
+{
+    public sealed class WindowsScreenTimeCounterServiceImple
+    {
+        private ScreenActivitySample _sample =
+            new ScreenActivitySample(DateTime.UtcNow, 0, null, null);
+
+        public int GetIdleTime()
+        {
+            return 0;
+        }
+
+        public DateTime GetLastActiveTimeUtc()
+        {
+            return DateTime.UtcNow;
+        }
+
+        public string? GetCurrActivePage()
+        {
+            return null;
+        }
+    }
+}
