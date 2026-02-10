0beb7bc77e25cc57d008bf8874a631ef56dabaf4
zidiyao <zidiyaocanada@outlook.com>
2026-02-10 02:47:56 -0500

    update pop up windows

--- web/dailygrid-ui/src/app/app.routes.ts
+++ web/dailygrid-ui/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PopupMonth } from './features/popup-month/popup-month';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PopupMonth }
+];
--- web/dailygrid-ui/src/app/app.ts
+++ web/dailygrid-ui/src/app/app.ts
@@ -1,31 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Todo } from './models/todo.model';
-import { TodoFormComponent } from './todo-form/todo-form';
-import { TodoItemComponent } from './todo-item/todo-item';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, TodoFormComponent, TodoItemComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css']   
 })
-export class App {
-  private nextId = 1;
-  todos: Todo[] = [];
-
-  onAdd(text: string) {
-    this.todos = [...this.todos, { id: this.nextId++, text, done: false }];
-  }
-
-  onToggle(id: number) {
-    this.todos = this.todos.map(t => t.id === id ? { ...t, done: !t.done } : t);
-  }
-
-  onRemove(id: number) {
-    this.todos = this.todos.filter(t => t.id !== id);
-  }
-
-  trackById = (_: number, t: Todo) => t.id;
-}
+export class App {}
